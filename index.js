2aafad14e92d81b2f8015897063ab953538a9e50
aimee goddard <aimeegoddard@aimees-MacBook-Pro.local>
2021-11-11 09:11:31 +0000

    boop

--- index.js
+++ index.js
@@ -6,3 +6,18 @@ function add(numOne, numTwo) {
     return numOne + numTwo
 }
 console.log(add(76,54));
+
+function multiply(numThree, numFour) {
+    return numThree * numFour
+}
+console.log(multiply(5,3))
+
+function sayHello(name) {
+    return ('Hello', name)
+}
+console.log(sayHello('Aimee'));
+
+function subtract(numOne, numTwo) {
+    return numOne - numTwo
+}
+console.log(subtract(40,23));
